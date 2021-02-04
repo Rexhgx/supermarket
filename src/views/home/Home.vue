c5064dee55375daf3caae0736d104b6330a0e63b
Rex <rexhgx@gmail.com>
2021-02-04 17:41:21 +0800

    Create NavBar components

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>Home</div>
+  <div id="home">
+    <NavBar class="home-nav">
+      <template v-slot:center>
+        <div>购物车</div>
+      </template>
+    </NavBar>
+  </div>
 </template>
 
 <script>
+  import NavBar from "components/common/navbar/NavBar";
+
   export default {
-    name: "Home"
+    name: "Home",
+    components: {
+      NavBar,
+    }
   }
 </script>
 
 <style scoped>
-
+  .home-nav{
+    background-color: var(--color-tint);
+    color: white;
+  }
 </style>
